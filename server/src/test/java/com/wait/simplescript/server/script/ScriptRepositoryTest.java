cb09b664fe3979160606ae76e788c7dccd240ad4
collinewait <collinewait17@gmail.com>
2020-05-16 14:26:41 +0300

    clean up context in ScriptRepositoryTest

--- server/src/test/java/com/wait/simplescript/server/script/ScriptRepositoryTest.java
+++ server/src/test/java/com/wait/simplescript/server/script/ScriptRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DataMongoTest
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles(SpringProfiles.TEST)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class ScriptRepositoryTest {
     @Autowired
     private ScriptRepository repository;
